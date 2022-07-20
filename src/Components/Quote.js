728aba38f6d681a09be2c9955ac4dd6349481f50
mod771 <70249026+mod771@users.noreply.github.com>
2022-07-20 18:20:08 -0500

    added styles to main content and button

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,7 +1,7 @@
 export default function Quote(props){
     return(
         <main>
-            <h2 className="adviveNumber">advice #{props.number}</h2>
+            <h2 className="adviceNumber">advice #{props.number}</h2>
             <h1 className="quote">"{props.quote}"</h1>
         </main>
     )
